9114937b57f54129613cc525864956147b993fa4
Sergio Francisco <sergiofsfilho@gmail.com>
2024-10-24 21:19:48 -0300

    Add 🏓

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/ping", methods=["GET"])
 def health_check():
-    return "<p>Pong!</p>"
+    return "<p>Pong! 🏓</p>"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=False)
